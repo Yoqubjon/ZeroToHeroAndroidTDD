635fd2502b673d1c7a1c4cfa3efbd32d986b7f04
Yoqub Shoimardonov <yoqubdeveloper@gmail.com>
2023-11-21 10:16:16 +0500

    task 011 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,42 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var count: Int = 0
+
+    companion object {
+        private const val COUNT_EXTRA = "count_extra"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rootLayout = findViewById<LinearLayout>(R.id.rootLayout)
+        val textView = findViewById<TextView>(R.id.countTextView)
+        val button = findViewById<Button>(R.id.incrementButton)
+
+        if (savedInstanceState != null) {
+            count = savedInstanceState.getInt(COUNT_EXTRA)
+        }
+        textView.text = count.toString()
+
+        button.setOnClickListener {
+            count += 2
+            textView.text = count.toString()
+        }
+
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(COUNT_EXTRA, count)
+        super.onSaveInstanceState(outState)
     }
 }
